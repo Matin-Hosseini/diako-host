ded8ed4120e4075748c1848029062a22472caacd
Matin-Hosseini <matinpinterest2003@gmail.com>
2023-07-28 23:58:38 +0330

    q-and-a half completed

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const mobileMenu = document.getElementById("mobile-menu");
 
+//q-and-a accordion
+const accordionsContainer = document.querySelector(".q-and-a__accordion");
+
 //hamburger menu starts
 let hamburgerMenuOpen = false;
 hamburgerMenu.addEventListener("click", () => {
@@ -11,3 +14,13 @@ hamburgerMenu.addEventListener("click", () => {
     : (mobileMenu.style.height = "0");
 });
 //hamburger menu ends
+
+//q-and-a accordion starts
+accordionsContainer.addEventListener("click", (e) => {
+  const { target } = e;
+  const accordionDescription = document.querySelector(
+    ".accordion__description"
+  );
+  console.log(target)
+});
+//q-and-a accordion ends
